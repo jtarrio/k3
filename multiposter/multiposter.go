cf2d6f6f618343664f48756322ee54b5e9555dd4
Jacobo Tarrio <jtarrio@gmail.com>
2025-03-12 22:29:13 -0400

    Add a 'multiposter' that can publish a list of posts as a sequence or thread

---
+++ multiposter/multiposter.go
@@ -0,0 +1,122 @@
+package multiposter
+
+import (
+	"context"
+
+	"github.com/bluesky-social/indigo/api/atproto"
+	"github.com/bluesky-social/indigo/api/bsky"
+	"github.com/jtarrio/atp/client"
+)
+
+// Multiposter is an interface to post multiple messages at once, either as a sequence of individual messages or as a thread.
+type Multiposter interface {
+	// Publish sends a series of posts to Bluesky.
+	Publish(ctx context.Context, posts []*bsky.FeedPost) *PublishResult
+	// Resume sends a series of posts to Bluesky, picking up where a previous call to Publish failed.
+	Resume(ctx context.Context, previousResult *PublishResult) *PublishResult
+}
+
+// PublishResult contains the result of a Publish or Resume operation.
+type PublishResult struct {
+	// Published contains the result of publishing each individual post. If the operation failed at some point,
+	// the results of the successful operations are here. Posts are always published serially, so if you tried
+	// to publish 10 posts and post number 4 failed, you will see the results of posts 1-3 here.
+	Published []*client.PublishResult
+	// Remaining contains the list of all posts that are left to be published. As an example, if you tried
+	// to publish 10 posts and post number 4 failed, you will see posts 4-10 here.
+	Remaining []*bsky.FeedPost
+	// Error contains the error returned by the last publish operation, if any.
+	Error error
+}
+
+// New creates a new Multiposter with the given client and options.
+//
+// By default, posts are published as a sequence of posts, but you can use the AsThread option to publish them as a thread.
+func New(client client.Client, options ...MultiposterOption) Multiposter {
+	m := &multiposter{client: client}
+	for _, option := range options {
+		option(m)
+	}
+	return m
+}
+
+// AsThread indicates that the posts will be published as a thread, with the first post as its root.
+func AsThread() MultiposterOption {
+	return func(m *multiposter) {
+		m.threaded = true
+	}
+}
+
+// AsSequence indicates that the posts will be published as a sequence of individual posts.
+//
+// The first post in the list will be published first, but note that the actual ordering
+// of the posts in the Bluesky view may depend on the order of the dates on the individual posts,
+// if it differs from the posting order.
+//
+// This is the default option.
+func AsSequence() MultiposterOption {
+	return func(m *multiposter) {
+		m.threaded = false
+	}
+}
+
+type MultiposterOption func(*multiposter)
+
+type multiposter struct {
+	client   client.Client
+	threaded bool
+}
+
+func (m multiposter) Publish(ctx context.Context, posts []*bsky.FeedPost) *PublishResult {
+	return m.doPublish(ctx, posts, nil)
+}
+
+func (m multiposter) Resume(ctx context.Context, previousResult *PublishResult) *PublishResult {
+	return m.doPublish(ctx, previousResult.Remaining, previousResult.Published)
+}
+
+func (m multiposter) doPublish(ctx context.Context, posts []*bsky.FeedPost, previousResults []*client.PublishResult) *PublishResult {
+	var threadRoot, threadParent *client.PublishResult
+	if len(previousResults) > 0 {
+		threadRoot = previousResults[0]
+		threadParent = previousResults[len(previousResults)-1]
+	}
+	result := &PublishResult{Published: previousResults}
+	for i := range posts {
+		var thisPost *bsky.FeedPost
+		if m.threaded && threadParent != nil && threadRoot != nil {
+			thisPost = setReplyField(posts[i], threadParent, threadRoot)
+		} else {
+			thisPost = posts[i]
+		}
+		singleResult, err := m.client.Publish(ctx, thisPost)
+		if err != nil {
+			result.Remaining = posts[i:]
+			result.Error = err
+			return result
+		}
+		threadParent = singleResult
+		if threadRoot == nil {
+			threadRoot = singleResult
+		}
+		result.Published = append(result.Published, singleResult)
+	}
+	return result
+}
+
+func setReplyField(thisPost *bsky.FeedPost, threadParent *client.PublishResult, threadRoot *client.PublishResult) *bsky.FeedPost {
+	postCopy := *thisPost
+	postCopy.Reply = &bsky.FeedPost_ReplyRef{
+		Parent: &atproto.RepoStrongRef{
+			LexiconTypeID: "com.atproto.repo.strongRef",
+			Cid:           threadParent.Cid,
+			Uri:           threadParent.Uri,
+		},
+		Root: &atproto.RepoStrongRef{
+			LexiconTypeID: "com.atproto.repo.strongRef",
+			Cid:           threadRoot.Cid,
+			Uri:           threadRoot.Uri,
+		},
+	}
+	return &postCopy
+}
---
+++ multiposter/multiposter_test.go
@@ -0,0 +1,233 @@
+package multiposter_test
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/bluesky-social/indigo/api/atproto"
+	"github.com/bluesky-social/indigo/api/bsky"
+	"github.com/jtarrio/atp"
+	"github.com/jtarrio/atp/client"
+	"github.com/jtarrio/atp/multiposter"
+	"github.com/jtarrio/atp/posts"
+	atptesting "github.com/jtarrio/atp/testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPostSequence(t *testing.T) {
+	c := &fakeClient{failAfter: -1}
+	m := multiposter.New(c)
+	posts := getPosts(10)
+	result := m.Publish(context.Background(), posts)
+	expectedResult := &multiposter.PublishResult{}
+	for i := range 10 {
+		expectedResult.Published = append(expectedResult.Published, &client.PublishResult{
+			Uri: uriOf(i),
+			Cid: cidOf(i),
+		})
+	}
+	assert.Equal(t, expectedResult, result)
+	var expectedPublished []publishedPost
+	for i := range posts {
+		expectedPublished = append(expectedPublished, publishedPost{
+			cid:  cidOf(i),
+			uri:  uriOf(i),
+			post: posts[i],
+		})
+	}
+	assert.Equal(t, expectedPublished, c.posts)
+}
+
+func TestResumeSequence(t *testing.T) {
+	c := &fakeClient{failAfter: 3}
+	m := multiposter.New(c)
+	posts := getPosts(10)
+	result := m.Publish(context.Background(), posts)
+	expectedResult := &multiposter.PublishResult{}
+	for i := range 3 {
+		expectedResult.Published = append(expectedResult.Published, &client.PublishResult{
+			Uri: uriOf(i),
+			Cid: cidOf(i),
+		})
+	}
+	expectedResult.Remaining = posts[3:]
+	expectedResult.Error = errPublish
+	assert.Equal(t, expectedResult, result)
+	assert.Len(t, c.posts, 3)
+
+	c.failAfter = -1
+
+	result2 := m.Resume(context.Background(), result)
+	expectedResult = &multiposter.PublishResult{}
+	for i := range 10 {
+		expectedResult.Published = append(expectedResult.Published, &client.PublishResult{
+			Uri: uriOf(i),
+			Cid: cidOf(i),
+		})
+	}
+	assert.Equal(t, expectedResult, result2)
+	var expectedPublished []publishedPost
+	for i := range posts {
+		expectedPublished = append(expectedPublished, publishedPost{
+			cid:  cidOf(i),
+			uri:  uriOf(i),
+			post: posts[i],
+		})
+	}
+	assert.Equal(t, expectedPublished, c.posts)
+}
+
+func TestPostThread(t *testing.T) {
+	c := &fakeClient{failAfter: -1}
+	m := multiposter.New(c, multiposter.AsThread())
+	posts := getPosts(10)
+	result := m.Publish(context.Background(), posts)
+	expectedResult := &multiposter.PublishResult{}
+	for i := range 10 {
+		expectedResult.Published = append(expectedResult.Published, &client.PublishResult{
+			Uri: uriOf(i),
+			Cid: cidOf(i),
+		})
+	}
+	assert.Equal(t, expectedResult, result)
+	var expectedPublished []publishedPost
+	for i := range posts {
+		postWithReply := *posts[i]
+		if i > 0 {
+			postWithReply.Reply = &bsky.FeedPost_ReplyRef{
+				Parent: &atproto.RepoStrongRef{
+					LexiconTypeID: "com.atproto.repo.strongRef",
+					Cid:           cidOf(i - 1),
+					Uri:           uriOf(i - 1),
+				},
+				Root: &atproto.RepoStrongRef{
+					LexiconTypeID: "com.atproto.repo.strongRef",
+					Cid:           cidOf(0),
+					Uri:           uriOf(0),
+				},
+			}
+		}
+		expectedPublished = append(expectedPublished, publishedPost{
+			cid:  cidOf(i),
+			uri:  uriOf(i),
+			post: &postWithReply,
+		})
+	}
+	assert.Equal(t, expectedPublished, c.posts)
+}
+
+func TestResumeThread(t *testing.T) {
+	c := &fakeClient{failAfter: 3}
+	m := multiposter.New(c, multiposter.AsThread())
+	posts := getPosts(10)
+
+	result := m.Publish(context.Background(), posts)
+	expectedResult := &multiposter.PublishResult{}
+	for i := range 3 {
+		expectedResult.Published = append(expectedResult.Published, &client.PublishResult{
+			Uri: uriOf(i),
+			Cid: cidOf(i),
+		})
+	}
+	expectedResult.Remaining = posts[3:]
+	expectedResult.Error = errPublish
+	assert.Equal(t, expectedResult, result)
+	assert.Len(t, c.posts, 3)
+
+	c.failAfter = -1
+
+	result2 := m.Resume(context.Background(), result)
+	expectedResult = &multiposter.PublishResult{}
+	for i := range 10 {
+		expectedResult.Published = append(expectedResult.Published, &client.PublishResult{
+			Uri: uriOf(i),
+			Cid: cidOf(i),
+		})
+	}
+	assert.Equal(t, expectedResult, result2)
+	var expectedPublished []publishedPost
+	for i := range posts {
+		postWithReply := *posts[i]
+		if i > 0 {
+			postWithReply.Reply = &bsky.FeedPost_ReplyRef{
+				Parent: &atproto.RepoStrongRef{
+					LexiconTypeID: "com.atproto.repo.strongRef",
+					Cid:           cidOf(i - 1),
+					Uri:           uriOf(i - 1),
+				},
+				Root: &atproto.RepoStrongRef{
+					LexiconTypeID: "com.atproto.repo.strongRef",
+					Cid:           cidOf(0),
+					Uri:           uriOf(0),
+				},
+			}
+		}
+		expectedPublished = append(expectedPublished, publishedPost{
+			cid:  cidOf(i),
+			uri:  uriOf(i),
+			post: &postWithReply,
+		})
+	}
+	assert.Equal(t, expectedPublished, c.posts)
+}
+
+func getPosts(count int) []*bsky.FeedPost {
+	converter := posts.NewConverter(posts.WithClock(&atptesting.FakeClock{Time: time.Date(2025, time.January, 2, 12, 34, 56, 789000000, time.UTC)}))
+	var out []*bsky.FeedPost
+	for i := range count {
+		post := atp.NewPost().AddText(fmt.Sprintf("This is post #%d", i+1))
+		out = append(out, converter.ToFeedPost(post))
+	}
+	return out
+}
+
+type fakeClient struct {
+	posts     []publishedPost
+	failAfter int
+}
+
+type publishedPost struct {
+	cid  string
+	uri  string
+	post *bsky.FeedPost
+}
+
+var errPublish = errors.New("failure posting")
+
+func (f *fakeClient) Publish(ctx context.Context, post *bsky.FeedPost) (*client.PublishResult, error) {
+	if f.failAfter == 0 {
+		return nil, errPublish
+	}
+	pp := publishedPost{
+		cid:  cidOf(len(f.posts)),
+		uri:  uriOf(len(f.posts)),
+		post: post,
+	}
+	f.posts = append(f.posts, pp)
+	if f.failAfter > 0 {
+		f.failAfter--
+	}
+	return &client.PublishResult{
+		Uri: pp.uri,
+		Cid: pp.cid,
+	}, nil
+}
+
+func cidOf(i int) string {
+	return fmt.Sprintf("%d", i)
+}
+
+func uriOf(i int) string {
+	return fmt.Sprintf("at://xxxx/yyyy/%d", i)
+}
+
+func (f *fakeClient) FindUserByHandle(ctx context.Context, handle string) (*client.UserData, error) {
+	panic("unimplemented")
+}
+
+func (f *fakeClient) GetAccessToken(ctx context.Context) error {
+	panic("unimplemented")
+}
